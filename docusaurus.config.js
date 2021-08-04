c80fe2dd81de8c1c78c0c2ef18ae4fa2ccff8451
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-08-04 14:44:56 +0900

    문서 전용모드 설정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,84 +1,86 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "이도현",
+  tagline: "Dinosaurs are cool",
+  url: "https://ksmfou98.github.io",
+  baseUrl: "/today-i-learn/",
+  organizationName: "ksmfou98", // Usually your GitHub org/user name.
+  projectName: "today-i-learn", // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: "이도현",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "이도현 Logo",
+        src: "img/logo.svg",
       },
       items: [
+        // {
+        //   type: "doc",
+        //   docId: "today-i-learn",
+        //   // to: "/docs/today-i-learn",
+        //   position: "left",
+        //   label: "Today I Learn",
+        // },
+
+        // { to: "/blog", label: "Diary", position: "left" },
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/ksmfou98",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
+    // footer: {
+    //   style: "dark",
+    //   links: [
+    //     {
+    //       title: "Docs",
+    //       items: [
+    //         {
+    //           label: "Tutorial",
+    //           to: "/docs/intro",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "Community",
+    //       items: [
+    //         {
+    //           label: "Stack Overflow",
+    //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
+    //         },
+    //         {
+    //           label: "Discord",
+    //           href: "https://discordapp.com/invite/docusaurus",
+    //         },
+    //         {
+    //           label: "Twitter",
+    //           href: "https://twitter.com/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: "More",
+    //       items: [
+    //         {
+    //           label: "Blog",
+    //           to: "/blog",
+    //         },
+    //         {
+    //           label: "GitHub",
+    //           href: "https://github.com/facebook/docusaurus",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    // },
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
@@ -86,22 +88,23 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
+          routeBasePath: "/", // 문서 전용 으로 설정
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
