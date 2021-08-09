377a7b2ba5d5d6ef1ed8a9ad13937009286daafb
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-08-09 15:21:25 +0900

    Docs : 고차함수 : forEach

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,86 +1,34 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "이도현",
-  tagline: "Dinosaurs are cool",
-  url: "https://ksmfou98.github.io",
-  baseUrl: "/today-i-learn/",
-  organizationName: "ksmfou98", // Usually your GitHub org/user name.
-  projectName: "today-i-learn", // Usually your repo name.
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  title: '이도현',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://ksmfou98.github.io',
+  baseUrl: '/today-i-learn/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ksmfou98', // Usually your GitHub org/user name.
+  projectName: 'today-i-learn', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "도현위키",
+      title: '도현위키',
       logo: {
-        alt: "도현위키 Logo",
-        src: "img/logo.svg",
+        alt: '도현위키 Logo',
+        src: 'img/logo.svg',
       },
       items: [
-        // {
-        //   type: "doc",
-        //   docId: "today-i-learn",
-        //   // to: "/docs/today-i-learn",
-        //   position: "left",
-        //   label: "Today I Learn",
-        // },
-
-        // { to: "/blog", label: "Diary", position: "left" },
+        // { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: "https://github.com/ksmfou98",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/ksmfou98',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
-    // footer: {
-    //   style: "dark",
-    //   links: [
-    //     {
-    //       title: "Docs",
-    //       items: [
-    //         {
-    //           label: "Tutorial",
-    //           to: "/docs/intro",
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: "Community",
-    //       items: [
-    //         {
-    //           label: "Stack Overflow",
-    //           href: "https://stackoverflow.com/questions/tagged/docusaurus",
-    //         },
-    //         {
-    //           label: "Discord",
-    //           href: "https://discordapp.com/invite/docusaurus",
-    //         },
-    //         {
-    //           label: "Twitter",
-    //           href: "https://twitter.com/docusaurus",
-    //         },
-    //       ],
-    //     },
-    //     {
-    //       title: "More",
-    //       items: [
-    //         {
-    //           label: "Blog",
-    //           to: "/blog",
-    //         },
-    //         {
-    //           label: "GitHub",
-    //           href: "https://github.com/facebook/docusaurus",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    // },
+
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
@@ -88,24 +36,24 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          routeBasePath: "/", // 문서 전용 으로 설정
-          editUrl: "https://github.com/ksmfou98/today-i-learn/edit/master/",
+          routeBasePath: '/', // 문서 전용 으로 설정
+          editUrl: 'https://github.com/ksmfou98/today-i-learn/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
   ],
-};
+}
