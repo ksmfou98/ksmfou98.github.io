88aa2391b01ef9430854e7d32c0d90bae22df06f
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-12-08 12:51:32 +0900

    utteranc 다크모드,라이트모드 지원

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,16 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import useThemeContext from '@theme/hooks/useThemeContext';
+
+const utterancesSelector = 'iframe.utterances-frame';
 
 function Comment() {
+  const { isDarkTheme } = useThemeContext();
+  const utterancesTheme = isDarkTheme ? 'github-dark' : 'github-light';
   const containerRef = useRef(null);
 
   useEffect(() => {
+    const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+
     const createUtterancesEl = () => {
       const script = document.createElement('script');
 
@@ -11,15 +18,22 @@ function Comment() {
       script.setAttribute('repo', 'ksmfou98/ksmfou98.github.io');
       script.setAttribute('issue-term', 'title');
       script.setAttribute('label', 'comment');
-      script.setAttribute('theme', 'github-light');
+      script.setAttribute('theme', utterancesTheme);
       script.crossOrigin = 'anonymous';
       script.async = true;
 
       containerRef.current.appendChild(script);
     };
+    const postThemeMessage = () => {
+      const message = {
+        type: 'set-theme',
+        theme: utterancesTheme,
+      };
+      utterancesEl.contentWindow.postMessage(message, 'https://utteranc.es');
+    };
 
-    createUtterancesEl();
-  }, []);
+    utterancesEl ? postThemeMessage() : createUtterancesEl();
+  }, [utterancesTheme]);
 
   return <div ref={containerRef} />;
 }
